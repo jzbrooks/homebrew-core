7ee8c1adb8b922e374de5615a5c9d87522938172
Justin Brooks <justin@jzbrooks.com>
2023-03-06 09:27:48 -0600

    swift-format 0.50700.1

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -2,10 +2,9 @@ class SwiftFormat < Formula
   desc "Formatting technology for Swift source code"
   homepage "https://github.com/apple/swift-format"
   url "https://github.com/apple/swift-format.git",
-      tag:      "0.50700.0",
-      revision: "3dd9b517b9e9846435aa782d769ef5825e7c2d65"
+      tag:      "0.50700.1",
+      revision: "5f184220d032a019a63df457cdea4b9c8241e911"
   license "Apache-2.0"
-  revision 1
   version_scheme 1
   head "https://github.com/apple/swift-format.git", branch: "main"
 
