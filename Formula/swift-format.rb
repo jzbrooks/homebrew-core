93e77cad7b20044a9851a2b35f20543dc3de2597
Justin Brooks <justin@jzbrooks.com>
2023-03-10 20:36:35 -0600

    Remove unused variable

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -30,7 +30,7 @@ class SwiftFormat < Formula
   uses_from_macos "swift"
 
   def install
-    system "swift", "build", "--disable-sandbox", "-c", "release", *swift_rpath
+    system "swift", "build", "-c", "release"
     bin.install ".build/release/swift-format"
     doc.install "Documentation/Configuration.md"
   end
